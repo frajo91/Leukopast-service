36adf482bcf2fbcd4763921a70a45b5d083d1457
frajo91 <frajo91@hotmail.com>
2025-04-10 19:00:24 -0500

    update multidominio

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Request;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +23,15 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Schema::defaultStringLength(191);
+        // Dinámicamente establece APP_URL basado en la solicitud actual
+        if (!app()->environment("local")) {
+            // Solo en producción/staging
+            URL::forceRootUrl(
+                Request::getSchemeAndHttpHost() . "/IntroduccionMarsiServicio"
+            );
+
+            // Opcional: Si tu instalación está en una subcarpeta (ej: /servicio)
+            // URL::forceRootUrl(Request::getSchemeAndHttpHost() . '/servicio');
+        }
     }
 }
