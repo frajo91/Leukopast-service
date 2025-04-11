195621f47512ca40b047c26b9f4e82ff117c0b19
frajo91 <frajo91@hotmail.com>
2025-04-10 19:00:24 -0500

    update multidominio

--- app/Http/Resources/RegistroResource.php
+++ app/Http/Resources/RegistroResource.php
@@ -14,31 +14,45 @@ class RegistroResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        $hayavance=false;
+        $hayavance = false;
         if (!empty($this->progreso)) {
-            $progreso=collect(json_decode(json_decode($this->progreso)));
-            $hayavance=true;
-            $lessons=collect(json_decode(json_encode($progreso['lessons']),true));
+            $progreso = collect(json_decode(json_decode($this->progreso)));
+            $hayavance = true;
+            $lessons = collect(
+                json_decode(json_encode($progreso["lessons"]), true)
+            );
         }
-                return [
-                    'IDUSUARIO' => $this->usuario->id,
-                    'NOMBRE' => $this->usuario->name,
-                    'APELLIDO' => $this->usuario->lastname,
-                    'FARMACIA' => $this->usuario->business,
-                    'CLIENTE' => $this->usuario->client,
-                    'CARGO' => $this->usuario->position,
-                    'CORREO' => $this->usuario->email,
-                    'TELEFONO' => $this->usuario->phone,
-                    'PAIS' => $this->usuario->country,
-                    'CIUDAD' => $this->usuario->city,
-                    'FECHA' => $this->updated_at,
-                    'PORCENTAJE_CURSO' => ($hayavance) ? $progreso['p']: 0 ,
-                    'PORCENTAJE_PRUEBA' => ($hayavance) ? (isset($lessons['4']['s']))?$lessons['5']['s']: 0 : 0,
-                    'ESTADO_PRUEBA' => ($hayavance) ? (isset($lessons['4']['s']))?($lessons['5']['s']>=$lessons['4']['ps'])?'Aprobada':'Reprobada': 'No registra' : 'No registra',
-                    'MODULOS' => ($hayavance) ? $lessons->keys()->map(function (int $item, int $key) {
-                            return $item +1;
-                        }): [] ,
-                    'ENVIO'=>$this->enviado,
-                ];
+        return [
+            "IDUSUARIO" => $this->usuario->id,
+            "NOMBRE" => $this->usuario->name,
+            "APELLIDO" => $this->usuario->lastname,
+            "FARMACIA" => $this->usuario->business,
+            "CLIENTE" => $this->usuario->client,
+            "CARGO" => $this->usuario->position,
+            "CORREO" => $this->usuario->email,
+            "TELEFONO" => $this->usuario->phone,
+            "PAIS" => $this->usuario->country,
+            "CIUDAD" => $this->usuario->city,
+            "FECHA" => $this->updated_at,
+            "PORCENTAJE_CURSO" => $hayavance ? $progreso["p"] : 0,
+            "PORCENTAJE_PRUEBA" => $hayavance
+                ? (isset($lessons["4"]["s"])
+                    ? $lessons["4"]["s"]
+                    : 0)
+                : 0,
+            "ESTADO_PRUEBA" => $hayavance
+                ? (isset($lessons["4"]["s"])
+                    ? ($lessons["4"]["s"] >= $lessons["4"]["ps"]
+                        ? "Aprobada"
+                        : "Reprobada")
+                    : "No registra")
+                : "No registra",
+            "MODULOS" => $hayavance
+                ? $lessons->keys()->map(function (int $item, int $key) {
+                    return $item + 1;
+                })
+                : [],
+            "ENVIO" => $this->enviado,
+        ];
     }
 }
